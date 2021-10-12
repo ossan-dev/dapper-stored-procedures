093f5396db19f1e4522c3cce8eefad940382de8b
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-10-12 13:23:26 +0200

    add utils method to manage dynamic object creation

---
+++ dapper-stored-procedures-app/dapper-stored-procedures/Utils/ExpandoObjectUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_stored_procedures.Utils
+{
+    public static class ExpandoObjectUtils
+    {
+        // https://www.oreilly.com/content/building-c-objects-dynamically/
+        public static void AddProperty(ExpandoObject obj, string key, object value)
+        {
+            // ExpandoObject supports IDictionary so we can extend it like this
+            var expandoDict = obj as IDictionary<string, object>;
+            if (expandoDict.ContainsKey(key))
+                expandoDict[key] = value;
+            else
+                expandoDict.Add(key, value);
+        }
+    }
+}
