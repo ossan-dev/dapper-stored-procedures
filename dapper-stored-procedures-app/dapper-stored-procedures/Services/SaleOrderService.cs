09b3fc13ed8f8864f97534573e5462a6ae7355a1
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-10-13 10:38:46 +0200

    make use of expando objects also in the sale order service

--- dapper-stored-procedures-app/dapper-stored-procedures/Services/SaleOrderService.cs
+++ dapper-stored-procedures-app/dapper-stored-procedures/Services/SaleOrderService.cs
@@ -1,9 +1,11 @@
 ﻿using Dapper;
 using dapper_stored_procedures.ApiModels;
 using dapper_stored_procedures.Models;
+using dapper_stored_procedures.Utils;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,14 +34,31 @@ namespace dapper_stored_procedures.Services
 
         public async Task<IEnumerable<SaleOrder>> GetSaleOrdersPagedAndFiltered(ComplexPaging complexPaging)
         {
+            var parameters = new DynamicParameters();
+            dynamic objArgs = new ExpandoObject();
+            ExpandoObjectUtils.AddProperty(objArgs, "offset", (complexPaging.PageNumber - 1) * complexPaging.ItemsPerPage);
+            ExpandoObjectUtils.AddProperty(objArgs, "take", complexPaging.ItemsPerPage);
+
             var orderBy = string.Join(',', complexPaging.SortPredicates.Select(s => $"{s.Field} {s.Direction}").ToArray());
+            if (!string.IsNullOrEmpty(orderBy))
+                ExpandoObjectUtils.AddProperty(objArgs, "orderBy", orderBy);
+
             string predicate = string.Empty;
             if (complexPaging.FilterPredicates.Count() > 0)
             {
-                predicate = $" WHERE {string.Join(' ', complexPaging.FilterPredicates.Select(f => $"{f.LogicalOperator} {f.Field} {f.RelationalOperator} {f.LiteralToCompare}"))}";
+                predicate = $" WHERE{string.Join(' ', complexPaging.FilterPredicates.Select(f => $"{f.LogicalOperator} {f.Field} {f.RelationalOperator} @{f.Field}"))}";
+
+                foreach (var filter in complexPaging.FilterPredicates)
+                {
+                    ExpandoObjectUtils.AddProperty(objArgs, filter.Field, filter.LiteralToCompare);
+                }
+
+                ExpandoObjectUtils.AddProperty(objArgs, "predicate", predicate);
             }
 
-            var saleOrders = await _connection.QueryAsync<SaleOrder>("usp.get_sale_order_complex_paging", new { offset = (complexPaging.PageNumber - 1) * complexPaging.ItemsPerPage, take = complexPaging.ItemsPerPage, orderBy = orderBy, predicate = predicate}, commandType: CommandType.StoredProcedure);
+            parameters.AddDynamicParams(objArgs);
+
+            var saleOrders = await _connection.QueryAsync<SaleOrder>("usp.get_sale_order_complex_paging", param: parameters, commandType: CommandType.StoredProcedure);
             return saleOrders;
         }
     }
